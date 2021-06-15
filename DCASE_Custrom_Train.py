afe8cd9bd699b04ec7c31f74c5ae5432ebf85970
अच्युत मणि त्रिपाठी <achyutmani9@gmail.com>
2021-06-15 15:20:48 +0530

    Update and rename LCustomdataset_Train.py to DCASE_Custrom_Train.py

--- DCASE_Custrom_Train.py
+++ DCASE_Custrom_Train.py
@@ -5,24 +5,18 @@ import h5py
 from torch.utils.data import dataset
 import pandas as pd 
 from PIL import Image
-class LAEData_Train():
+class Sound_Data_Train():
 	def __init__(self,transform=None):
-		self.annotations=np.array(pd.read_csv("TrainFile.csv"))# Read The names of Training Signals
+		self.annotations=np.array(pd.read_csv("TrainFile.csv"))# Read The CSV file WIth Names of Training Signals and Class Labels
 		self.transform=transform
 	def __len__(self):
 		return len(self.annotations)
 	def __getitem__(self,index):
 		key=self.annotations[index,0]
 		#print(index)
-		with h5py.File('LDCASE2019.hdf5', 'r') as f: # Database for Left Channel Training Spectrogram
+		with h5py.File('LDCASE2019.hdf5', 'r') as f: # H5.py file That Contains Spectrogram Features for All The Training Signals
 			SG_Data = f[key][()]
-			#SG_Data=np.array(SG_Data)
 			SG_Data=Image.fromarray(SG_Data)
-			#SG_Data=np.log((SG_Data + 1e-10))
-			#for i in range(len(SG_Data)):
-				#SG_Data[i,:]=SG_Data[i,:]/np.sum(SG_Data[i,:])
-			#SGmax,SGmin=SG_Data.max(),SG_Data.min()
-			#SG_Data=(SG_Data-SGmin)/(SGmax-SGmin)
 			SG_Label= torch.from_numpy(np.array(self.annotations[index,1]))
 			if self.transform:
 				SG_Data=self.transform(SG_Data)
